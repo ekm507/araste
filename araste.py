819050f0384b3b0e2f6489811ee5c8421d5723ea
Erfan Kheyrollahi <ekm507@gmail.com>
2022-07-06 01:04:17 +0430

    add korsi support

--- araste.py
+++ araste.py
@@ -1,4 +1,4 @@
-from font2 import f1
+from font3 import f1, korsi
 
 def copyboard(blockstr, cursor, board):
     block = [list(line) for line in blockstr.split('\n')]
@@ -8,7 +8,7 @@ def copyboard(blockstr, cursor, board):
         for j in range(lsize):
             board[i][cursor - lsize + j] = block[i][j];
 
-    return board, len(block[5])
+    return board, len(block[korsi])
 
 after_n = list("رذزدژاءوؤ!؟?\n. ‌،:؛")
 before_n = list(" ‌،؛:.؟!?\n")
@@ -17,9 +17,9 @@ fa = list('ضصثقفغعهخحجچشسیبلاتنمکگظطزرذدپوؤءژ'
 def render(text, boardw, boardh, empty_char = ' '):
     board = [ [empty_char for i in range(boardw)] for j in range(boardh)]
     # text = '\n'.join(''.join(line) for line in board)
-    cursor = boardw - 1
+    cursor = boardw
     text = ' ' + text + ' '
-    for i in range(len(text)):
+    for i in range(1, len(text) - 1):
         z = text[i]
         if text[i] in fa:
             if text[i+1] not in before_n:
@@ -27,21 +27,22 @@ def render(text, boardw, boardh, empty_char = ' '):
                     z = z + 'ـ'
             if text[i-1] not in after_n:
                 z = 'ـ' + z
+        
 
         board, lenc = copyboard(f1[z], cursor, board)
-        print(z, lenc)
+        # print(z, lenc)
         cursor -= lenc
 
 
     for line in board:
-        print(''.join(line))
+        print(''.join(line[cursor:]))
 
 text = 'ببب پ بابا'
 # text = 'ا'
 
 from sys import argv
 text = argv[1]
-render(text, 100, 8, '.')
+render(text, 100, 10, '.')
 # qa = f1['ا'].split('\n')
 # qb = f1['ب'].split('\n')
 # qr = f1['ر'].split('\n')
