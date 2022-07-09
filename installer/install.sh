609e8727cfb52a87025f9387d02fec887f45f9c9
sameet <sameetmoosavi@yahoo.com>
2022-07-09 14:55:26 +0430

    update installer script

--- installer/install.sh
+++ installer/install.sh
@@ -7,13 +7,10 @@ function msg() {
  printf "%s\n" "$text"
 }
 function main(){
- if ! [[ -d "$HOME/.local/bin" ]];then
-   mkdir $HOME/.local/bin
- fi
  msg "cloning repo"
  sudo git clone https://github.com/ekm507/araste.git /usr/share/araste
  msg "installing binary file"
- sudo cp /home/sameet/Repos/araste/araste.py /bin/araste
+ sudo cp /usr/share/araste/araste.py /bin/araste
  sudo chmod +x /bin/araste
 }
 main
